58399edf980bed7650250fc2720e6683578d633c
Muhammad-Sharjeel-Asif <m.sharjeelasif1435@gmail.com>
2025-10-28 01:48:08 +0500

    version-2

--- 05_Extract_Data_from_File/Extract_Data.java
+++ 05_Extract_Data_from_File/Extract_Data.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Extract_Data {
@@ -8,86 +9,92 @@ public class Extract_Data {
             File myFile = new File("EVE01Sales.txt");
             Scanner reader = new Scanner(myFile);
 
-            int row = 0, Total = 0, E011 = 0, E012 = 0, E013 = 0, E014 = 0, ProductA = 0, ProductB = 0, ProductC = 0,
-                    ProductD = 0;
+            System.out.println("=== Auto Generated Menu ===");
 
             String data = reader.nextLine();
+            String[] heading = data.split("\t");
+            for (int i = 0; i < heading.length - 2; i++) {
+                System.out.println(i + 1 + "." + heading[i]);
+            }
 
-            // Now data is saved in form of array
-            String[] sepData = data.split("\t");
+            int TotalSales = 0;
+            HashMap<String, Integer> empSales = new HashMap<>();
+            HashMap<String, Integer> prodSales = new HashMap<>();
+            HashMap<String, Integer> regSales = new HashMap<>();
+            HashMap<String, Integer> MonthSales = new HashMap<>();
 
             while (reader.hasNextLine()) {
                 data = reader.nextLine();
 
-                sepData = data.split("\t");
+                String[] sepData = data.split("\t");
+
+                String empId = sepData[2];
+                String prodId = sepData[3];
+                String regId = sepData[1];
+                String monthId = sepData[0];
 
                 int qty = Integer.parseInt(sepData[4]);
                 int price = Integer.parseInt(sepData[5]);
+                int rowPrice = qty * price;
 
-                // Total Sales Amount
-                int rowTotal = qty * price;
-                Total += rowTotal;
+                // Employee-wise Sales Amount
+                TotalSales += rowPrice;
 
-                // Employee-wise Sales Amount:
+                // Employee-wise Sales Amount
+                int prevEmpSales = empSales.getOrDefault(empId, 0);
+                empSales.put(empId, prevEmpSales + rowPrice);
 
-                // Employee E011
-                if (sepData[2].equals("E011")) {
-                    rowTotal = qty * price;
-                    E011 += rowTotal;
-                }
-                // Employee E012
-                else if (sepData[2].equals("E012")) {
-                    rowTotal = qty * price;
+                // Product-wise Sales Amount
+                int prevProdSales = prodSales.getOrDefault(prodId, 0);
+                prodSales.put(prodId, prevProdSales + rowPrice);
 
-                    E012 += rowTotal;
-                }
-                // Employee E013
-                else if (sepData[2].equals("E013")) {
-                    rowTotal = qty * price;
-                    E013 += rowTotal;
-                }
-                // Employee E014
-                else if (sepData[2].equals("E014")) {
-                    rowTotal = qty * price;
-                    E014 += rowTotal;
-                }
+                // Region-wise Sales Amount
+                int previousRegSales = regSales.getOrDefault(regId, 0);
+                regSales.put(regId, previousRegSales + rowPrice);
 
-                // Product-Wise Sales Amount:
+                // Month-wise Sales Amount
+                String[] monthArray = monthId.split("-");
+                String month = monthArray[1];
 
-                // Product A
-                if (sepData[3].equals("Product A")) {
-                    rowTotal = qty * price;
-                    ProductA += rowTotal;
-                }
-                // Product B
-                else if (sepData[3].equals("Product B")) {
-                    rowTotal = qty * price;
-                    ProductB += rowTotal;
-                }
-                // Product C
-                else if (sepData[3].equals("Product C")) {
-                    rowTotal = qty * price;
-                    ProductC += rowTotal;
-                }
-                // Product D
-                else if (sepData[3].equals("Product D")) {
-                    rowTotal = qty * price;
-                    ProductD += rowTotal;
+                int prevMonthSales = MonthSales.getOrDefault(month, 0);
+                MonthSales.put(month, prevMonthSales + rowPrice);
+            }
+
+            System.out.println("\n");
+
+            Scanner user = new Scanner(System.in);
+            System.out.print("Select to see Sales Data: ");
+            int userIn = Integer.parseInt(user.nextLine());
+            System.out.println("\n");
+
+            if (userIn == 1) {
+                for (String month : MonthSales.keySet()) {
+                    System.out.println("The Sales Amount of " + month + " is Rs." +
+                            MonthSales.get(month));
                 }
             }
-            System.out.println("The Sales Total Amount is Rs." + Total);
 
-            System.out.println("\n");
-            System.out.println("The Sales Amount is Employee E011 is Rs." + E011);
-            System.out.println("The Sales Amount is Employee E012 is Rs." + E012);
-            System.out.println("The Sales Amount is Employee E013 is Rs." + E013);
-            System.out.println("The Sales Amount is Employee E014 is Rs." + E014);
-            System.out.println("\n");
+            else if (userIn == 2) {
+                for (String reg : regSales.keySet()) {
+                    System.out.println("The Sales Amount of " + reg + " is Rs." + regSales.get(reg));
+                }
+            }
 
-            System.out.println("The Sales Amount is Employee Product A is Rs." + ProductA);
-            System.out.println("The Sales Amount is Employee Product B is Rs." + ProductB);
-            System.out.println("The Sales Amount is Employee Product C is Rs." + ProductC);
-            System.out.println("The Sales Amount is Employee Product D is Rs." + ProductD);
+            else if (userIn == 3) {
+                for (String emp : empSales.keySet()) {
+                    System.out.println("The Sales Amount of Employee " + emp + " is Rs." + empSales.get(emp));
+                }
+            } else if (userIn == 4) {
+                for (String product : prodSales.keySet()) {
+                    System.out.println("The Sales Amount of " + product + " is Rs." + prodSales.get(product));
+                }
+            } else {
+                System.out.println("The Total Sales Amount is Rs." + TotalSales);
+            }
+
+            if (userIn <= 4 && userIn >= 1)
+                System.out.println("\n");
+            System.out.println("The Total Sales Amount is Rs." + TotalSales);
 
         } catch (Exception e) {
             System.out.println(e);
